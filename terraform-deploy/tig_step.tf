e3431d8284b0fd9579dd2fbc71636fa10c5212ef
Simon Liu <simon.liu@jpl.nasa.gov>
2025-08-04 12:13:39 -0700

    /deploy sit

--- terraform-deploy/tig_step.tf
+++ terraform-deploy/tig_step.tf
@@ -71,7 +71,7 @@ resource "aws_sfn_state_machine" "tig" {
           "task_config": {
             "collection": "{$.meta.collection}",
             "buckets": "{$.meta.buckets}",
-            "role_mappings": "${aws_ssm_parameter.role_mappings.name}",
+            "role_mappings": "${aws_ssm_parameter.role_mappings.value}",
             "cumulus_message": {
               "input": "{$.payload}"
             }
