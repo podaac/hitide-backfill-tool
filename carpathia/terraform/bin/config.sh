9340a6e5dfff1e9295c2addb2fb048095bda764b
Simon Liu <simon.liu@jpl.nasa.gov>
2025-08-12 16:44:41 -0700

    test

--- carpathia/terraform/bin/config.sh
+++ carpathia/terraform/bin/config.sh
@@ -3,24 +3,17 @@ set -eo pipefail
 
 if [ ! $# -eq 1 ]
 then
-    echo "$(caller | cut -d' ' -f2) venue"
+    echo "usage: $(caller | cut -d' ' -f2) venue"
     exit 1
 fi
 
 VENUE=$1
-shift
-
-cd "$(dirname $BASH_SOURCE)/../"
-source "environments/$VENUE.env"
+source "$(dirname $BASH_SOURCE)/../environments/$VENUE.env"
 
 export TF_IN_AUTOMATION=true  # https://www.terraform.io/cli/config/environment-variables#tf_in_automation
 export TF_INPUT=false  # https://www.terraform.io/cli/config/environment-variables#tf_input
 
 export TF_VAR_region="$REGION"
 export TF_VAR_stage="$VENUE"
-
-echo "VENUE: $VENUE"
-echo "REGION: $REGION"
-echo "BUCKET: $BUCKET"
 
 terraform init -reconfigure -backend-config="bucket=$BUCKET" -backend-config="region=$REGION"
